5777e2873693458d0be8bd142b25ab7426fe7537
Rafał <rafi20302@gmail.com>
2025-11-19 12:05:03 +0100

    woriking on callback

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,15 +3,15 @@ import "./App.css";
 import { axiosInstance } from "./lib/api/axios";
 
 function App() {
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
 
-  useEffect(() => {
-    axiosInstance
-      .get("/api/rest/v1/data")
-      .then((response) => setData(response.data));
-  }, []);
+  // useEffect(() => {
+  //   axiosInstance
+  //     .get("/api/rest/v1/data")
+  //     .then((response) => setData(response.data));
+  // }, []);
 
-  return <div>Data: {JSON.stringify(data)}</div>;
+  return <div>Data: Data</div>;
 }
 
 export default App;
