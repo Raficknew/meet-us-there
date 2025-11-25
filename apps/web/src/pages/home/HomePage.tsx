fae799ee253549cd2beb98597a3cca7cb71e0da7
Rafał <rafi20302@gmail.com>
2025-11-25 19:52:05 +0100

    added persist in auth store

--- apps/web/src/pages/home/HomePage.tsx
+++ apps/web/src/pages/home/HomePage.tsx
@@ -12,8 +12,6 @@ export default function HomePage() {
     }
   }, [user]);
 
-  console.log("Current user:", user);
-
   return (
     <div>
       {user?.name}
--- apps/web/src/store/useAuthStore.tsx
+++ apps/web/src/store/useAuthStore.tsx
@@ -2,6 +2,7 @@ import type { UserResponse } from "@repo/shared/responses";
 import { create } from "zustand";
 import { axiosInstance } from "../lib/api/axios";
 import { UsersService } from "../lib/api/Users.service";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 export interface AuthState {
   user: UserResponse | null;
@@ -11,28 +12,41 @@ export interface AuthState {
   logout: () => void;
 }
 
-export const useAuthStore = create<AuthState>((set) => ({
-  user: null,
-  isLoading: true,
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      user: null,
+      isLoading: true,
 
-  handleAccessToken: async (token: string) => {
-    axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      handleAccessToken: async (token: string) => {
+        axiosInstance.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${token}`;
 
-    try {
-      const user = await UsersService.getMyUser();
-      console.log("Fetched user:", user);
-      set({ user, isLoading: false });
-    } catch (error) {
-      console.error(error);
+        try {
+          const user = await UsersService.getMyUser();
+          set({ user, isLoading: false });
+        } catch (error) {
+          console.error(error);
 
-      set({ user: null, isLoading: false });
+          set({ user: null, isLoading: false });
+        }
+      },
+
+      logout: () => {
+        document.cookie =
+          "access_token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/; SameSite=None; Secure";
+        axiosInstance.defaults.headers.common["Authorization"] = "";
+        set({ user: null });
+      },
+    }),
+    {
+      name: "auth-store",
+      partialize: (state) =>
+        Object.fromEntries(
+          Object.entries(state).filter(([key]) => key !== "isLoading")
+        ),
+      storage: createJSONStorage(() => localStorage),
     }
-  },
-
-  logout: () => {
-    document.cookie =
-      "access_token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/; SameSite=None; Secure";
-    axiosInstance.defaults.headers.common["Authorization"] = "";
-    set({ user: null });
-  },
-}));
+  )
+);
